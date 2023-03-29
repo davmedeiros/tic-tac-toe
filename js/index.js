605ffda5b9064530afb7672b971ba794f4b5e5d9
David Medeiros <123705989+dav-med@users.noreply.github.com>
2023-03-29 10:24:29 +0100

    Create gameBoard function

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const gameBoard = (() => {
+  const grid = [];
+  const size = 9;
+
+  const add = (position, symbol) => {
+    if (position < 0 || position > size) {
+      return `Enter a position between 0 and ${size}.`;
+    }
+
+    if (symbol !== 'X' || symbol !== 'O') {
+      return 'Enter a valid symbol [X, O].'
+    }
+
+    if (grid[position] !== undefined) {
+      return `The position ${position} is already filled.`
+    }
+
+    grid.splice(position, 1, symbol);
+  };
+})();
