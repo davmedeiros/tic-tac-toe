56122603ea9813c079057c3c4808cc32fde60962
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-07 19:37:26 +0100

    Create DisplayController module

--- js/index.js
+++ js/index.js
@@ -5,4 +5,17 @@ const GameBoard = (() => {
     return { getBoard };
 })();
 
+const DisplayController = (() => {
+    const board = document.querySelector('#board');
 
+    const showBoard = () => {
+        GameBoard.getBoard().forEach(value => {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.textContent = value;
+            board.appendChild(cell);
+        })
+    }
+
+    return { showBoard };
+})();
