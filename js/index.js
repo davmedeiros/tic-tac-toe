b6c6c69d84e45aa84b2b1df17768fcb5a0c21d4a
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-07 23:34:39 +0100

    Setup winning conditions

--- js/index.js
+++ js/index.js
@@ -30,7 +30,12 @@ const Game = (() => {
 
         if (board[0] && board[0] === board[1] && board[1] === board[2] ||
             board[3] && board[3] === board[4] && board[4] === board[5] ||
-            board[6] && board[6] === board[7] && board[7] === board[8]) {
+            board[6] && board[6] === board[7] && board[7] === board[8] ||
+            board[0] && board[0] === board[4] && board[4] === board[8] ||
+            board[6] && board[6] === board[6] && board[6] === board[2] ||
+            board[0] && board[0] === board[3] && board[3] === board[6] ||
+            board[1] && board[1] === board[4] && board[4] === board[7] ||
+            board[2] && board[2] === board[5] && board[5] === board[8]) {
             console.log('Winner');
         }
     }
