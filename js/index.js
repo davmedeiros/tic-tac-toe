608ace7542bdb1add3cd34f8e2965d6500efeab2
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-07 21:38:09 +0100

    Mark cells with an X (test) when hit.

--- js/index.js
+++ js/index.js
@@ -2,16 +2,31 @@ const GameBoard = (() => {
     const BOARD_SIZE = 9;
     const board = Array(BOARD_SIZE).fill('');
     const getBoard = () => board;
-    return { getBoard };
+
+    const markCell = (cell, symbol) => {
+        board[cell] = symbol;
+    }
+
+    return { getBoard, markCell };
 })();
 
 const DisplayController = (() => {
     const board = document.querySelector('#board');
 
+    const markCell = (cell, symbol) => {
+        GameBoard.markCell(cell, symbol);
+        cell.target.textContent = symbol;
+    }
+
     const showBoard = () => {
         GameBoard.getBoard().forEach(value => {
             const cell = document.createElement('div');
             cell.classList.add('cell');
+
+            cell.addEventListener('click', (cell) => {
+                markCell(cell, 'X');
+            })
+
             cell.textContent = value;
             board.appendChild(cell);
         })
