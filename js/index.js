3bdc48ccbff9cad14fc97d15a955d1f4b456b81d
David Medeiros <123705989+davmedeiros@users.noreply.github.com>
2023-08-08 22:31:53 +0100

    Fix wrong winning case condition

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ const Game = (() => {
             board[3] && board[3] === board[4] && board[4] === board[5] ||
             board[6] && board[6] === board[7] && board[7] === board[8] ||
             board[0] && board[0] === board[4] && board[4] === board[8] ||
-            board[6] && board[6] === board[6] && board[6] === board[2] ||
+            board[6] && board[6] === board[4] && board[4] === board[2] ||
             board[0] && board[0] === board[3] && board[3] === board[6] ||
             board[1] && board[1] === board[4] && board[4] === board[7] ||
             board[2] && board[2] === board[5] && board[5] === board[8]) {
